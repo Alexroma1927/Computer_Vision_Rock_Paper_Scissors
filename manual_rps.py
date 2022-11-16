169aff779619fe703dde56d961edaaceee8c9f16
Alexroma1927 <110851434+Alexroma1927@users.noreply.github.com>
2022-11-16 11:10:29 +0100

    Add files via upload
    Milestone 4 Task1

---
+++ manual_rps.py
@@ -0,0 +1,11 @@
+import random
+class Rps:
+     #Computer choice
+    #def _get_computer_choice():
+        List_1 = ['rock', 'paper', 'scissor']
+        print(random.choice(List_1))
+      # Player choice
+    #def _get_user_choice():
+        player = input('player ''write your choice: ')
+        list_2 = ['rock', 'paper', 'scissor']
+        print(player)
