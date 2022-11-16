96972f204d1483f6de54c4c1bb81c98b7f72d3f1
Alexroma1927 <110851434+Alexroma1927@users.noreply.github.com>
2022-11-16 11:20:55 +0100

    Milestone 4 task 2

--- manual_rps.py
+++ manual_rps.py
@@ -3,9 +3,23 @@ class Rps:
      #Computer choice
     #def _get_computer_choice():
         List_1 = ['rock', 'paper', 'scissor']
-        print(random.choice(List_1))
+        computer_choice = random.choice(List_1)
+        print(computer_choice)
       # Player choice
     #def _get_user_choice():
         player = input('player ''write your choice: ')
         list_2 = ['rock', 'paper', 'scissor']
         print(player)
+
+        while player == computer_choice:
+            print('It is a Tie.')
+            break
+        else:
+            if player == 'Scissor' and computer_choice =='Paper':
+                print('Player Wins')
+            elif player == 'Scissor' and computer_choice =='Rock':
+                print('Player_2 Wins')
+            else: 
+                player == 'Scissor' and computer_choice =='Scissor'
+                print('It is a Tie')
+            
